778bfba1d127b8550456c2b9747d17e6c60bc7ab
Rebecca Deprey <rdeprey@gmail.com>
2019-07-20 19:16:41 -0400

    Cleanup Firestore persistence logic.

--- src/App.js
+++ src/App.js
@@ -19,20 +19,13 @@ function App() {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
   });
 
+  // Enable Firestore database persistence so database can be queried/used offline
   firebase.firestore().enablePersistence()
-  .then(function(res) {
-    console.log(res);
-  })
   .catch(function(err) {
-    console.log(err);
       if (err.code === 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          // ...
+          console.log('Failed Firestore persistence pre-condition: ', err);
       } else if (err.code === 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-          // ...
+          console.log('Browser doesn\'t support features needed for Firestore persistence.');
       }
   });
 
